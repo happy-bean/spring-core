0efd9dee9ee3bc3839961d35d36817e5fabf3fea
wgt <18222997200@163.com>
2019-04-03 23:10:35 +0800

    @WebServlet 实现servlet配置

---
+++ src/main/java/org/happybean/spring/servlet/SimpleServlet.java
@@ -0,0 +1,24 @@
+package org.happybean.spring.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author wgt
+ * @date 2019-04-03
+ * @description
+ **/
+@WebServlet("/hello")
+public class SimpleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //@WebServlet 使用注解方式实现路由
+        //xml配置方式这里不做讲解
+        resp.getWriter().write("hello");
+    }
+}
